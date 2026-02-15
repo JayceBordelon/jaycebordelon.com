23bd8ab17f258f4651c480320c93411e9f24b960
Jayce Bordelon <b.jayce@wustl.edu>
2026-02-15 09:34:42 -0600

    Restructure into monorepo with personal site and trading bot services

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "@/components/hero";
+
+export default function page() {
+  return (
+    <main className="h-dvh overflow-hidden">
+      <HeroSection />
+    </main>
+  );
+}
